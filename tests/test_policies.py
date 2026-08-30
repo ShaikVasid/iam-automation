7db05d7fc96ba99e6b899101b074e778599bca89
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-29 21:46:43 -0400

    test: replace stale AWS policy test with Azure RBAC coverage

--- tests/test_policies.py
+++ tests/test_policies.py
@@ -1,8 +1,36 @@
-from iam_automation.policies import build_allow_policy
+from iam_automation.policies import build_role_assignment, normalize_scopes
 
 
-def test_build_allow_policy_deduplicates_values():
-    policy = build_allow_policy(["s3:GetObject", "s3:GetObject"], ["arn:aws:s3:::bucket/*"])
-    assert policy["Version"] == "2012-10-17"
-    assert policy["Statement"][0]["Action"] == ["s3:GetObject"]
-    assert policy["Statement"][0]["Resource"] == ["arn:aws:s3:::bucket/*"]
+def test_build_role_assignment_defaults_to_service_principal():
+    assignment = build_role_assignment(
+        principal_id="principal-123",
+        role_definition_id="role-reader",
+    )
+
+    assert assignment == {
+        "principalId": "principal-123",
+        "roleDefinitionId": "role-reader",
+        "principalType": "ServicePrincipal",
+    }
+
+
+def test_build_role_assignment_supports_custom_principal_type():
+    assignment = build_role_assignment(
+        principal_id="group-123",
+        role_definition_id="role-reader",
+        principal_type="Group",
+    )
+
+    assert assignment["principalType"] == "Group"
+
+
+def test_normalize_scopes_removes_duplicates_and_trailing_slashes():
+    scopes = normalize_scopes(
+        [
+            "/subscriptions/ABC/resourceGroups/Prod/",
+            "/subscriptions/abc/resourceGroups/prod",
+            "",
+        ]
+    )
+
+    assert scopes == ["/subscriptions/abc/resourcegroups/prod"]
